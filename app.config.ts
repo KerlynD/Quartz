2675be178ae0427a78b839ae658e77209b11b502
Kerlyn Difo <100390581+KerlynD@users.noreply.github.com>
2025-02-02 21:34:52 -0500

    chore: Move sensitive config files to /config directory and update gitignore

--- app.config.ts
+++ app.config.ts
@@ -11,7 +11,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ios: {
     supportsTablet: false,
     bundleIdentifier: 'com.kerlyn.quartz',
-    googleServicesFile: './GoogleService-Info.plist',
+    googleServicesFile: './config/GoogleService-Info.plist',
     config: {
       usesNonExemptEncryption: false
     },
